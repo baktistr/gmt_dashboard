7fe8c2953f58d59a59f2d703aafea0dc4ed798ce
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-07-23 20:08:54 +0800

    Add page label

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,21 @@
 <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
     <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button><a class="c-header-brand d-sm-none" href="#"><img class="c-header-brand" src="{{ env('APP_URL', '') }}/assets/brand/coreui-base.svg" width="97" height="46" alt="CoreUI Logo"></a>
     <button class="c-header-toggler c-class-toggler ml-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true"><span class="c-header-toggler-icon"></span></button>
+
+    <ul class="c-header-nav d-md-down-none">
+        @if(request()->routeIs('home'))
+            <li class="c-header-nav-item font-weight-bolder pl-2">Dashboard</li>
+        @endif
+
+        @if(request()->routeIs('treg.show'))
+            <li class="c-header-nav-item font-weight-bolder pl-2">{{ $treg->formatted_name }}</li>
+        @endif
+
+        @if(request()->routeIs('building.show'))
+            <li class="c-header-nav-item font-weight-bolder pl-2">{{ $building->name }}</li>
+        @endif
+    </ul>
+
     <ul class="c-header-nav ml-auto mr-4">
         <li class="c-header-nav-item dropdown">
             <a class="c-header-nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
--- resources/views/treg/show.blade.php
+++ resources/views/treg/show.blade.php
@@ -4,8 +4,6 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col">
-            <h3>Daftar Gedung {{ $treg->formatted_name }}</h3>
-
             <building-list :data='@json($assets)'></building-list>
         </div>
     </div>
