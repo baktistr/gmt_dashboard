994363e01a74605f94f563bc273a5e1a7ab9f0b3
luck_nut <hasyrawi@gmail.com>
2020-08-10 04:22:06 +0800

    space Available 10 monts Bar Chart query Data

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Asset;
+use App\BuildingSpace;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BuildingController extends Controller
@@ -15,8 +17,40 @@ class BuildingController extends Controller
      */
     public function show(Asset $building)
     {
+        $availableSpace = $this->availableSpaceChart($building->id);
+
         return view('building.show', [
-            'building' => $building,
+            'building'       => $building,
+            'availableSpace' => $availableSpace
         ]);
+    }
+
+    /**
+     * Space Available Space Bar Chart
+     * 
+     * @return Illuminate\Support\Collection
+     */
+    protected function availableSpaceChart($id)
+    {
+        $last10Months = collect([]);
+
+        for ($i = 0; $i < 10; $i++) {
+            $last10Months->add(Carbon::now()->subMonths($i));
+        }
+
+        $last10Months->transform(function ($month) use ($id) {
+            $availableSpace = BuildingSpace::query()
+                ->where('asset_id', $id)
+                ->where('is_available' , true)
+                ->whereMonth('created_at', $month)
+                ->count();
+
+            return [
+                'month' => $month->format('F Y'),
+                'data' => $availableSpace,
+            ];
+        });
+
+        return $last10Months;
     }
 }
