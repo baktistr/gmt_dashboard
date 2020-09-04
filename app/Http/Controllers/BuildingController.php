07007585ea69e69eb40d93f101acb8e104566700
luck_nut <hasyrawi@gmail.com>
2020-09-04 18:28:10 +0800

    make controller for counting chart

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Asset;
+use App\BuildingHelpDesk;
 use App\BuildingSpace;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class BuildingController extends Controller
 {
@@ -19,17 +21,28 @@ class BuildingController extends Controller
     {
         $availableSpace   = $this->availableSpaceChart($building->id)->toArray();
         $unAvailableChart = $this->availableSpaceChart($building->id, false)->toArray();
+        $pending          = $this->countComplaint('pending', $building->id);
+        $done             = $this->countComplaint('done', $building->id);
+        $progress         = $this->countComplaint('in-progress', $building->id);
+        $total            = $this->totalComplaint($building->id);
+        $complaintByMount = $this->complaintCountBymonth($building->id);
+
         return view('building.show', [
             'building'         => $building,
             'availableSpace'   => $availableSpace,
             'unAvailableChart' => $unAvailableChart,
+            'pending'          => $pending,
+            'done'             => $done,
+            'progress'         => $progress,
+            'total'            => $total,
+            'complaints'       => $complaintByMount,
         ]);
     }
 
     /**
      * Space Available Space Bar Chart
      * 
-     * @return Illuminate\Support\Collection
+     * @return Collection
      */
     protected function availableSpaceChart($id, $available = true)
     {
@@ -41,7 +54,7 @@ class BuildingController extends Controller
 
         $last10Months->transform(function ($month) use ($id, $available) {
             $availableSpace = BuildingSpace::query()
-                ->where('asset_id', $id)
+                ->where('building_id', $id)
                 ->where('is_available', $available)
                 ->whereMonth('created_at', $month)
                 ->count();
@@ -53,5 +66,65 @@ class BuildingController extends Controller
         });
 
         return $last10Months;
+    }
+
+    /**
+     * Display Complaint Chart
+     * @return int
+     */
+    protected function countComplaint(string $status, int $building_id = null)
+    {
+
+        $complaints = Asset::query()
+            ->with('complaints')
+            ->where('id', $building_id)
+            ->whereHas('complaints', function ($query) use ($status) {
+                $query->where('status', $status);
+            })->count();
+
+        return $complaints;
+    }
+
+    /**
+     * Count total Compalaint
+     * @return int
+     */
+    protected function totalComplaint(int $building_id)
+    {
+        /* Total Complaints */
+
+        $complaints = Asset::query()
+            ->find($building_id)
+            ->complaints
+            ->count();
+
+        return $complaints;
+    }
+
+    protected function complaintCountBymonth(int $building_id)
+    {
+
+        $last12Months = collect([]);
+
+        for ($i = 0; $i < 12; $i++) {
+            $last12Months->add(Carbon::now()->months($i + 1));
+        }
+
+        $last12Months->transform(function ($month) use ($building_id) {
+
+            $complaint = BuildingHelpDesk::query()
+                ->where('building_id', $building_id)
+                ->whereMonth('created_at', $month)
+                ->whereYear('created_at' , $month)
+                ->count();
+
+            return [
+                'data'   => $complaint,
+                'labels' => $month->format('F Y'),
+            ];
+        });
+
+        return $last12Months;
+
     }
 }
